dc65f7ff949611d3c7e21d40e1d857e5a62b5215
Muhammad Aditya <81363093+aditya3232@users.noreply.github.com>
2023-06-14 10:40:55 +0700

    many2many in odoo
    - menambahkan brand

--- module_custom/algoritma_pembelian/models/algoritma_pembelian.py
+++ module_custom/algoritma_pembelian/models/algoritma_pembelian.py
@@ -7,6 +7,7 @@ class algoritma_pembelian(models.Model):
     tanggal = fields.Date(string="Tanggal")
     status = fields.Selection([('draft','Draft'),('to_approve','To Approve'),('approved','Approved'),('done','Done')], default='draft')
     algoritma_pembelian_ids = fields.One2many('algoritma.pembelian.line', 'algoritma_pembelian_id', string="Algoritma Pembelian Ids")
+    brand_ids = fields.Many2many('algoritma.brand', 'algoritma_pembelian_brand_rel', 'algoritma_pembelian_id', 'brand_id', string='Brand Ids')
 
 class algoritma_pembelian_line(models.Model):
     _name = 'algoritma.pembelian.line'
@@ -15,3 +16,8 @@ class algoritma_pembelian_line(models.Model):
     product_id = fields.Many2one('product.product', string="Product Id")
     quantity = fields.Float(string="Quantity", default=0.0)
     uom_id = fields.Many2one('uom.uom', string="Uom Id")
+
+class algoritma_brand(models.Model):
+    _name = 'algoritma.brand'
+
+    name = fields.Char(string="Name")
